f8f490649c4e5c46cbac277b08be026ca4ad0315
fedespads <fedecraft643@gmail.com>
2023-10-10 09:26:15 +0200

    aggiornamento cors

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from "express";
+const express = require("express");
+const cors = require("cors"); // Importa il pacchetto CORS
 const app = express();
 const port = 9000;
 
-app.use("/",(req,res)=>{
-  res.json({message: "ha funzionatoo"});
-})
+app.use(cors()); // Abilita CORS per tutte le route
 
-app.listen(9000, ()=>{
-  console.log('start server in '+port)
-})
+app.use("/", (req, res) => {
+  res.json({ message: "ha funzionato" });
+});
+
+app.listen(9000, () => {
+  console.log('start server in ' + port)
+});
